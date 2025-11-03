ab68a129a95072bfc946ad1898e8043266d44a89
yurkimus <yurkimus@gmail.com>
2025-11-03 17:37:03 +0100

    Remove d.ts

--- source/index.js
+++ source/index.js
@@ -1,20 +1,11 @@
 /**
  * Returns a curried version of a function.
  *
- * @type {typeof import('./index.d.ts').curry}
- *
- * @param {Function} predicate
- * @param {number} length
+ * @param {(...parameters: any[]) => any} predicate
+ * @param {number} [length]
  *
  * @throws {TypeError} "predicate" must be a function
  * @throws {TypeError} "length" must be a number
- *
- * @example
- * ```javascript
- * let add = (a, b) => a + b
- *
- * curry(add)(1)(2) // => 3
- * ```
  */
 export let curry = (predicate, length = predicate.length) => {
   if (typeof predicate !== 'function')
